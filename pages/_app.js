d04bd9e712e0f08385558ae7c47cbb8847b3ba65
Ivanna Sevkovych <is@wacg.de>
2022-03-27 22:06:26 +0200

    Add Bosch font

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                {/* This ways to add css on global website use local asset folder withhtml link tag */}
+                <link
+                    href="/assets/fonts/bosch/Bosch.otf"
+                    rel="stylesheet"
+                />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
