9b8525a333fed182637522b38af3680a76b0a673
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2022-03-27 19:05:58 +0200

    Fix TextureLoader warning

--- components/ThreeIcon.js
+++ components/ThreeIcon.js
@@ -1,5 +1,5 @@
-import { useRef } from 'react'
-import { Canvas, useFrame, extend } from '@react-three/fiber'
+import { useRef, Suspense } from 'react'
+import { Canvas, useFrame, useLoader, extend } from '@react-three/fiber'
 import { shaderMaterial, OrbitControls } from '@react-three/drei'
 import * as THREE from 'three'
 import styles from '../styles/ThreeIcon.module.scss'
@@ -9,11 +9,13 @@ import fragment from './glsl/fragment.glsl'
 
 function IconPlane({ iconFile, ...threeProps }) {
 
+    const texture = useLoader(THREE.TextureLoader, `/assets/${iconFile}`)
+
     // Create a shader material
     const ThreeIconMaterial = shaderMaterial(
         {
             time: 0,
-            iconTexture: new THREE.TextureLoader().load(`/assets/${iconFile}`)
+            iconTexture: texture
         },
         vertex,
         fragment,
@@ -46,9 +48,11 @@ export const ThreeIcons = ({ iconFiles }) => {
     return (
         <Canvas className={styles.canvas} camera={{ position: [0, 0, 2] }}>
             <color attach="background" args={["#F4D566"]} />
-            {
-                iconFiles.map((iconFile, index) => <IconPlane key={index} position={[ index == 2 ? 0.5 : index, index == 2 ? 1 : 0, 0]} iconFile={iconFile} />)
-            }
+            <Suspense fallback={"<h1>Loading...</h1>"}>
+                {
+                    iconFiles.map((iconFile, index) => <IconPlane key={index} position={[index == 2 ? 0.5 : index, index == 2 ? 1 : 0, 0]} iconFile={iconFile} />)
+                }
+            </Suspense>
             <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
         </Canvas>
     )
