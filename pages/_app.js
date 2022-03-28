f20d3eb4177d63985d8987c4b45d7bcff81783dc
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2022-03-28 22:25:57 +0200

    Fix font warning (CSS-link in Head component)

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,7 @@
-import Head from "next/head";
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
-    return (
-        <>
-            <Head>
-                {/* This ways to add css on global website use local asset folder withhtml link tag */}
-                <link
-                    href="/assets/fonts/bosch/Bosch.otf"
-                    rel="stylesheet"
-                />
-            </Head>
-            <Component {...pageProps} />
-        </>
-    )
+    return <Component {...pageProps} />
 }
 
 export default MyApp
