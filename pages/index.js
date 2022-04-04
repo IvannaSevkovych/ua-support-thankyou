28be04ba401f217c8e70b5bed962b15ac15d208d
Ivanna Sevkovych <is@wacg.de>
2022-04-04 22:14:20 +0200

    Style mobile index page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Link from 'next/link'
 import Head from 'next/head'
-import Emoji from '../components/Emoji'
+import Image from 'next/image'
 
-import styles from '../styles/Home.module.css'
+import Emoji from '../components/Emoji'
+import { Wave } from "../components/Wave";
+
+import styles from '../styles/Home.module.scss'
 
 import participantIds from '../data/_ids'
 
@@ -16,37 +19,72 @@ export default function Home({ participantIds }) {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <main className={styles.main}>
-                <div>
-                    <h1 className={styles.title}>
-                        Familie Sevkovych Sagt Danke!
-                    </h1>
+            <main className={styles.container}>
 
-                    <p>
-                        Wir, eine seit vielen Jahren in Stuttgart <Emoji symbol='🇩🇪' label='Deutschland' /> lebende Familie aus Kyiv <Emoji symbol='🇺🇦' label='Ukraine' />, wollen uns bei sehr vielen von unseren Mitmenschen bedanken, die Zeit, Kraft und materielle Ressourcen zur Unterstützung der ukrainischen Bevölkerung aufgebracht haben und weiterhin aufbringen. Mit vereinten Kräften helfen wir alle das Elend des Krieges abzuschwächen und die Welt wieder ein Stückchen humaner und lebenswerter zu machen. Danke Euch vom ganzen Herzen <Emoji symbol='❤️' label='Herz' />!
-                    </p>
+                {/* Blue wrapper */}
+                <div className={styles.blue__wrapper}>
 
-                    <p>
-                        Manche von Euch haben unsere Hilfsaktion direkt unterstützt. Andere haben ihre eigenen, viel größeren Hilfsaktionen gestartet und wir haben uns wie ein kleines Zahnrädchen eingefügt. Um Euch allen unsere tiefste Dankbarkeit und Anerkennung zu zeigen, haben wir diese kleine Webseite auf die Beine gestellt. Wir hoffen Euch mit ein bisschen Spaß beim Durchklicken belohnen zu können. Klickt auf START um unserem kleinen Recap von Beginn an zu folgen. Oder springt zu der Euch interessierenden Seite mit den Links weiter unten.
-                    </p>
+                    {/* Overlay image */}
+                    <Image
+                        className={styles.blue__background}
+                        alt="Dima und Ivanna Sevkovych"
+                        src="/assets/photos/sevkovych_index_overlay.jpg"
+                        layout="fill"
+                        objectFit="cover"
+                        quality={100}
+                    />
+
+                    <div className={styles.blue__foreground}>
+                        {/* for aligning the h1 element */}
+                        <div></div>
+
+                        <h1 className={styles.title}>
+                            Дякуємо!
+                        </h1>
+
+                        <Link href="/thankyou/jb_steuerberatung">
+                            <a className={styles.link__wrapper}>
+                                <span>Start</span>
+                                <svg className={styles.link__arrow} viewBox="0 0 39 155" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path vectorEffect="non-scaling-stroke" d="M34 154L1.99999 77.5L34 1" strokeWidth="2" />
+                                    <path vectorEffect="non-scaling-stroke" d="M38 125L18 77.5L38 30" strokeWidth="1" />
+                                </svg>
+                            </a>
+                        </Link>
+
+                    </div>
+
+                    {/* Wave */}
+                    <Wave />
+
                 </div>
 
-                <div>
-                    {/* TODO: Section + call to action -> start button */}
-                </div>
+                {/* Yellow wrapper */}
+                <div className={styles.yellow__wrapper}>
 
-                <div>
-                    {
-                        participantIds.map(id => {
-                            return (
-                                <div key={id}>
-                                    <Link href="/thankyou/[id]" as={`/thankyou/${id}`}>
+                    <div>
+                        <h2 className={styles.subtitle}>Familie Sevkovych sagt: <span>Danke!</span></h2>
+                        <p>
+                            Wir, eine seit vielen Jahren in Stuttgart <Emoji symbol='🇩🇪' label='Deutschland' /> lebende Familie aus Kyiv <Emoji symbol='🇺🇦' label='Ukraine' />, wollen uns bei sehr vielen von unseren Mitmenschen bedanken, die Zeit, Kraft und materielle Ressourcen zur Unterstützung der ukrainischen Bevölkerung aufgebracht haben und weiterhin aufbringen. Mit vereinten Kräften helfen wir alle das Elend des Krieges abzuschwächen und die Welt wieder ein Stückchen humaner und lebenswerter zu machen. Danke Euch vom ganzen Herzen <Emoji symbol='❤️' label='Herz' />!
+                        </p>
+
+                        <p>
+                            Manche von Euch haben unsere Hilfsaktion direkt unterstützt. Andere haben ihre eigenen, viel größeren Hilfsaktionen gestartet und wir haben uns wie ein kleines Zahnrädchen eingefügt. Um Euch allen unsere tiefste Dankbarkeit und Anerkennung zu zeigen, haben wir diese kleine Webseite auf die Beine gestellt. Wir hoffen Euch mit ein bisschen Spaß beim Durchklicken belohnen zu können. Klickt auf START um unserem kleinen Recap von Beginn an zu folgen. Oder springt zu der Euch interessierenden Seite mit den Links weiter unten.
+                        </p>
+                    </div>
+
+                    <div className={styles.links_wrapper}>
+                        {
+                            participantIds.map(id => {
+                                return (
+                                    <Link key={id} href="/thankyou/[id]" as={`/thankyou/${id}`}>
                                         <a>{id}</a>
                                     </Link>
-                                </div>
-                            )
-                        })
-                    }
+                                )
+                            })
+                        }
+                    </div>
+
                 </div>
 
             </main>
