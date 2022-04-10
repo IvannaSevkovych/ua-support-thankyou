36fbcc2493002b77a36cc9240ff4589ee8978b47
Ivanna Sevkovych <is@wacg.de>
2022-04-10 14:18:49 +0200

    Add framer motion to the project

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Link from 'next/link'
 import Head from 'next/head'
 import Image from 'next/image'
+import { motion } from "framer-motion";
 
-import Emoji from '../components/Emoji'
 import { Wave } from "../components/Wave";
 import { HomeLink } from '../components/HomeLink';
 
@@ -12,78 +12,65 @@ import participantIds from '../data/_ids'
 
 export default function Home({ participants }) {
     return (
-        <div className={styles.container}>
-            <Head>
-                <title>TODO</title>
-                <meta name="description" content="TODO" />
-                {/* TODO */}
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-
-            <main className={styles.container}>
-
-                {/* Blue wrapper */}
-                <div className={styles.blue__wrapper}>
-
-                    {/* Overlay image */}
-                    <Image
-                        className={styles.blue__background}
-                        alt="Dima und Ivanna Sevkovych"
-                        src="/assets/photos/sevkovych_index_overlay.jpg"
-                        layout="fill"
-                        objectFit="cover"
-                        quality={100}
-                    />
-
-                    <div className={styles.blue__foreground}>
-                        {/* for aligning the h1 element */}
-                        <div></div>
-
-                        <h1 className={styles.title}>
-                            Дякуємо!
-                        </h1>
-
-                        <Link href="/thankyou/jb_steuerberatung">
-                            <a className={styles.link__wrapper}>
-                                <span>Start</span>
-                                <svg className={styles.link__arrow} viewBox="0 0 39 155" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path vectorEffect="non-scaling-stroke" d="M34 154L1.99999 77.5L34 1" strokeWidth="2" />
-                                    <path vectorEffect="non-scaling-stroke" d="M38 125L18 77.5L38 30" strokeWidth="1" />
-                                </svg>
-                            </a>
-                        </Link>
-
+        <motion.div exit={{opacity: 0}}>
+            <div className={styles.container}>
+                <Head>
+                    <title>TODO</title>
+                    <meta name="description" content="TODO" />
+                    {/* TODO */}
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
+                <main className={styles.container}>
+                    {/* Blue wrapper */}
+                    <div className={styles.blue__wrapper}>
+                        {/* Overlay image */}
+                        <Image
+                            className={styles.blue__background}
+                            alt="Dima und Ivanna Sevkovych"
+                            src="/assets/photos/sevkovych_index_overlay.jpg"
+                            layout="fill"
+                            objectFit="cover"
+                            quality={100}
+                        />
+                        <div className={styles.blue__foreground}>
+                            {/* for aligning the h1 element */}
+                            <div></div>
+                            <h1 className={styles.title} >
+                                Дякуємо!
+                            </h1>
+                            <Link href="/thankyou/jb_steuerberatung">
+                                <a className={styles.link__wrapper}>
+                                    <span>Start</span>
+                                    <svg className={styles.link__arrow} viewBox="0 0 39 155" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path vectorEffect="non-scaling-stroke" d="M34 154L1.99999 77.5L34 1" strokeWidth="2" />
+                                        <path vectorEffect="non-scaling-stroke" d="M38 125L18 77.5L38 30" strokeWidth="1" />
+                                    </svg>
+                                </a>
+                            </Link>
+                        </div>
+                        {/* Wave */}
+                        <Wave />
                     </div>
-
-                    {/* Wave */}
-                    <Wave />
-
-                </div>
-
-                {/* Yellow wrapper */}
-                <div className={styles.yellow__wrapper}>
-
-                    <div className={styles.subtitle__wrapper}>
-                        <h2 className={styles.subtitle}>Familie Sevkovych sagt: <span>Danke!</span></h2>
-                        <p>
-                            Wir, eine seit vielen Jahren in Stuttgart lebende Familie aus Kyiv, wollen uns bei sehr vielen von unseren Mitmenschen bedanken, die Zeit, Kraft und materielle Ressourcen zur Unterstützung der ukrainischen Bevölkerung aufgebracht haben und weiterhin aufbringen. Mit vereinten Kräften helfen wir alle das Elend des Krieges abzuschwächen und die Welt wieder ein Stückchen humaner und lebenswerter zu machen. Danke Euch vom ganzen Herzen!
-                        </p>
-
-                        <p>
-                            Manche von Euch haben unsere Hilfsaktion direkt unterstützt. Andere haben ihre eigenen, viel größeren Hilfsaktionen gestartet und wir haben uns wie ein kleines Zahnrädchen eingefügt. Um Euch allen unsere tiefste Dankbarkeit und Anerkennung zu zeigen, haben wir diese kleine Webseite auf die Beine gestellt. Wir hoffen Euch mit ein bisschen Spaß beim Durchklicken belohnen zu können. Klickt auf START um unserem kleinen Recap von Beginn an zu folgen. Oder springt zu der Euch interessierenden Seite mit den Links weiter unten.
-                        </p>
+                    {/* Yellow wrapper */}
+                    <div className={styles.yellow__wrapper}>
+                        <div className={styles.subtitle__wrapper}>
+                            <h2 className={styles.subtitle}>Familie Sevkovych sagt: <span>Danke!</span></h2>
+                            <p>
+                                Wir, eine seit vielen Jahren in Stuttgart lebende Familie aus Kyiv, wollen uns bei sehr vielen von unseren Mitmenschen bedanken, die Zeit, Kraft und materielle Ressourcen zur Unterstützung der ukrainischen Bevölkerung aufgebracht haben und weiterhin aufbringen. Mit vereinten Kräften helfen wir alle das Elend des Krieges abzuschwächen und die Welt wieder ein Stückchen humaner und lebenswerter zu machen. Danke Euch vom ganzen Herzen!
+                            </p>
+                            <p>
+                                Manche von Euch haben unsere Hilfsaktion direkt unterstützt. Andere haben ihre eigenen, viel größeren Hilfsaktionen gestartet und wir haben uns wie ein kleines Zahnrädchen eingefügt. Um Euch allen unsere tiefste Dankbarkeit und Anerkennung zu zeigen, haben wir diese kleine Webseite auf die Beine gestellt. Wir hoffen Euch mit ein bisschen Spaß beim Durchklicken belohnen zu können. Klickt auf START um unserem kleinen Recap von Beginn an zu folgen. Oder springt zu der Euch interessierenden Seite mit den Links weiter unten.
+                            </p>
+                        </div>
+                        <div className={styles.links_wrapper}>
+                            {
+                                participants.map(p => <HomeLink key={p.id} id={p.id} name={p.name} />)
+                            }
+                        </div>
                     </div>
-
-                    <div className={styles.links_wrapper}>
-                        {
-                            participants.map(p => <HomeLink key={p.id} id={p.id} name={p.name} />)
-                        }
-                    </div>
-
-                </div>
-
-            </main>
-        </div >
+                </main>
+            </div>
+        </motion.div>
     )
 }
 
--- pages/thankyou/[id].js
+++ pages/thankyou/[id].js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from 'next/image'
 import Link from 'next/link'
+import { motion } from "framer-motion";
 
 import { ThreeIcon } from "../../components/ThreeIcon";
 import { Arrow } from "../../components/Arrow";
@@ -13,96 +14,86 @@ import participantIds from '../../data/_ids'
 
 const Participant = ({ participant }) => {
     return (
-        <div className={styles.container}>
-            <Head>
-                <title>
-                    {participant.name}, danke!
-                </title>
-            </Head>
-
-            {/* Blue wrapper */}
-            <div className={styles.blue__wrapper}>
-
-                {/* Overlay image */}
-                <Image
-                    className={styles.blue__background}
-                    alt={participant.imgAlt}
-                    src={`/assets/photos/${participant.id}.jpg`}
-                    layout="fill"
-                    objectFit="cover"
-                    quality={100}
-                />
-
-                <div className={styles.blue__foreground}>
-                    {/* Desktop prev arrow */}
-                    {
-                        participant.prevPage && <Arrow display='desktop' prev={participant.prevPage} />
-                    }
-                    {/* Text content with home navigation */}
-                    <div className={styles.blue__content}>
-                        <Link href="/">
-                            <a className={styles.home__link}><Emoji symbol='🇺🇦' label='Ukraine' /> &nbsp; Zur Startseite</a>
-                        </Link>
-
-                        <div className={styles.headline__wrapper}>
-                            {/* Mobile prev arrow */}
-                            {
-                                participant.prevPage && <Arrow display='mobile' prev={participant.prevPage} />
-                            }
-                            <h1>{participant.name}</h1>
-                            {/* Mobile next arrow with placeholder */}
-                            <Arrow display='mobile' next={participant.nextPage} />
+        <motion.div exit={{opacity: 0}}>
+            <div className={styles.container}>
+                <Head>
+                    <title>
+                        {participant.name}, danke!
+                    </title>
+                </Head>
+                {/* Blue wrapper */}
+                <div className={styles.blue__wrapper}>
+                    {/* Overlay image */}
+                    <Image
+                        className={styles.blue__background}
+                        alt={participant.imgAlt}
+                        src={`/assets/photos/${participant.id}.jpg`}
+                        layout="fill"
+                        objectFit="cover"
+                        quality={100}
+                    />
+                    <div className={styles.blue__foreground}>
+                        {/* Desktop prev arrow */}
+                        {
+                            participant.prevPage && <Arrow display='desktop' prev={participant.prevPage} />
+                        }
+                        {/* Text content with home navigation */}
+                        <div className={styles.blue__content}>
+                            <Link href="/">
+                                <a className={styles.home__link}><Emoji symbol='🇺🇦' label='Ukraine' /> &nbsp; Zur Startseite</a>
+                            </Link>
+                            <div className={styles.headline__wrapper}>
+                                {/* Mobile prev arrow */}
+                                {
+                                    participant.prevPage && <Arrow display='mobile' prev={participant.prevPage} />
+                                }
+                                <h1>{participant.name}</h1>
+                                {/* Mobile next arrow with placeholder */}
+                                <Arrow display='mobile' next={participant.nextPage} />
+                            </div>
+                            <div className={styles.texts__wrapper}>
+                                {
+                                    participant.texts.map((text, index) => <div key={index} className={styles.text} dangerouslySetInnerHTML={{ __html: text }} />)
+                                }
+                            </div>
                         </div>
-
-                        <div className={styles.texts__wrapper}>
-                            {
-                                participant.texts.map((text, index) => <div key={index} className={styles.text} dangerouslySetInnerHTML={{ __html: text }} />)
-                            }
-                        </div>
-
                     </div>
+                    {/* Wave */}
+                    <Wave />
                 </div>
-
-                {/* Wave */}
-                <Wave />
-
-            </div>
-
-            {/* Yellow wrapper */}
-            <div className={styles.yellow__wrapper}>
-
-                {/* Canvases */}
-                <div className={styles.canvas__wrapper}>
+                {/* Yellow wrapper */}
+                <div className={styles.yellow__wrapper}>
+                    {/* Canvases */}
+                    <div className={styles.canvas__wrapper}>
+                        {
+                            participant.iconFiles.map((iconFile, index) => <ThreeIcon display='desktop' key={index} iconFile={iconFile} iconIndex={index} iconsTotal={participant.iconFiles.length} />)
+                        }
+                        {
+                            participant.iconFiles.map((iconFile, index) => <ThreeIcon display='mobile' key={index} iconFile={iconFile} iconIndex={index} iconsTotal={participant.iconFiles.length} />)
+                        }
+                    </div>
+                    {/* Desktop next arrow */}
                     {
-                        participant.iconFiles.map((iconFile, index) => <ThreeIcon display='desktop' key={index} iconFile={iconFile} iconIndex={index} iconsTotal={participant.iconFiles.length} />)
-                    }
-                    {
-                        participant.iconFiles.map((iconFile, index) => <ThreeIcon display='mobile' key={index} iconFile={iconFile} iconIndex={index} iconsTotal={participant.iconFiles.length} />)
+                        participant.nextPage && <Arrow display='desktop' next={participant.nextPage} />
                     }
                 </div>
-
-                {/* Desktop next arrow */}
-                {
-                    participant.nextPage && <Arrow display='desktop' next={participant.nextPage} />
-                }
+                {/* <div>
+                    {
+                        participant.prevPage && <Link href="/thankyou/[id]" as={`/thankyou/${participant.prevPage}`}>
+                            <a>&larr;</a>
+                        </Link>
+                    }
+                    <Link href="/">
+                        <a>Home</a>
+                    </Link>
+                    {
+                        participant.nextPage && <Link href="/thankyou/[id]" as={`/thankyou/${participant.nextPage}`}>
+                            <a>&rarr;</a>
+                        </Link>
+                    }
+                </div> */}
             </div>
-
-            {/* <div>
-                {
-                    participant.prevPage && <Link href="/thankyou/[id]" as={`/thankyou/${participant.prevPage}`}>
-                        <a>&larr;</a>
-                    </Link>
-                }
-                <Link href="/">
-                    <a>Home</a>
-                </Link>
-                {
-                    participant.nextPage && <Link href="/thankyou/[id]" as={`/thankyou/${participant.nextPage}`}>
-                        <a>&rarr;</a>
-                    </Link>
-                }
-            </div> */}
-        </div>
+        </motion.div>
     )
 }
 
