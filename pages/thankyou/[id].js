8cb3106293efe5ed44ce6cd5263431c2bf03949b
Ivanna Sevkovych <is@wacg.de>
2022-05-23 21:06:15 +0200

    Fix typo

--- pages/thankyou/[id].js
+++ pages/thankyou/[id].js
@@ -37,7 +37,7 @@ const Participant = ({ participant, imageProps }) => {
                             layout="fill"
                             objectFit="cover"
                             quality={40}
-                            priorityr
+                            priority
                         />
                     </motion.div>
                     <div className={styles.blue__foreground}>
