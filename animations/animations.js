e837a867a0c5e0518a141bb361ec96e4524229f0
Ivanna Sevkovych <is@wacg.de>
2022-05-26 17:19:48 +0200

    Test:remove translate animation

--- animations/animations.js
+++ animations/animations.js
@@ -1,11 +1,9 @@
-export const pageTransition = {
-    hidden: { opacity: 1, x: 0, y: 0 },
-    enter: { opacity: 1, x: '100%', y: 0 },
-    exit: { opacity: 1, x: 0, y: 0 },
-}
-
 export const textTransition = {
-    hidden: { opacity: 0, x: 0, y: 100 },
-    enter: { opacity: 1, x: 0, y: 0 },
-    exit: { opacity: 0, x: 0, y: 100 },
+    hidden: { opacity: 0 },
+    enter: { opacity: 1 },
+    exit: { opacity: 0 }
+
+    // hidden: { opacity: 0, x: 0, y: 100 },
+    // enter: { opacity: 1, x: 0, y: 0 },
+    // exit: { opacity: 0, x: 0, y: 100 },
 }
