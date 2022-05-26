00d2605d90f5da89c0a10ef9d67d8b5ceec7b5c1
Ivanna Sevkovych <is@wacg.de>
2022-05-26 17:34:45 +0200

    Test: bring back exit animation

--- animations/animations.js
+++ animations/animations.js
@@ -1,7 +1,10 @@
 export const textTransition = {
-    hidden: { opacity: 0, x: 0, y: 100 },
-    enter: { opacity: 1, x: 0, y: 0 },
-    // exit: { opacity: 0 }
+    hidden: { y: 100, opacity: 0, },
+    enter: {
+        y: 0,
+        opacity: 1,
+    },
+    exit: { y: 100, opacity: 0, }
 
     // hidden: { opacity: 0, x: 0, y: 100 },
     // enter: { opacity: 1, x: 0, y: 0 },
