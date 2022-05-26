f0bcc4860238b063369210ed9e6e4d8d41d5202a
Ivanna Sevkovych <is@wacg.de>
2022-05-26 17:23:54 +0200

    Test:remove exit animation

--- animations/animations.js
+++ animations/animations.js
@@ -1,7 +1,7 @@
 export const textTransition = {
-    hidden: { opacity: 0 },
-    enter: { opacity: 1 },
-    exit: { opacity: 0 }
+    hidden: { opacity: 0, x: 0, y: 100 },
+    enter: { opacity: 1, x: 0, y: 0 },
+    // exit: { opacity: 0 }
 
     // hidden: { opacity: 0, x: 0, y: 100 },
     // enter: { opacity: 1, x: 0, y: 0 },
