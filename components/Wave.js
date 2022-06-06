c869f115e502c435f390d0a069e6a7a5475863a3
Ivanna Sevkovych <is@wacg.de>
2022-06-06 12:46:11 +0200

    Replace inline svg with Next Image

--- components/Wave.js
+++ components/Wave.js
@@ -1,4 +1,8 @@
+import Image from 'next/image'
+
 import styles from '../styles/Wave.module.scss'
+
+import desktop from '../public/assets/wave/wave_desktop.svg';
 
 export const Wave = () => {
 
@@ -21,22 +25,16 @@ export const Wave = () => {
                 </path>
             </svg>
 
-            <svg className={styles.wave__desktop} viewBox="0 0 158 1080" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path>
-                    <animate attributeName="d"
-                        dur="7s"
-                        repeatCount="indefinite"
-                        values="
-                    M6.86056 657.5C23.8076 551.674 190.861 162.5 138.779 1.52588e-05L158 0V1080H121.304C15.8602 951.5 -15.6393 798 6.86056 657.5Z;
-                    M51.9993 414.5C68.9463 308.675 137.781 186 137.781 1.52588e-05L157.002 0V1080H70.4999C137.781 803.5 29.4994 555 51.9993 414.5Z;
-                    M130.5 414.5C147.447 308.675 71.6366 186 71.6366 1.52588e-05L158.138 0V1080H71.6366C17.5 779 108 555 130.5 414.5Z;
 
-                    M6.86056 657.5C23.8076 551.674 190.861 162.5 138.779 1.52588e-05L158 0V1080H121.304C15.8602 951.5 -15.6393 798 6.86056 657.5Z;
-                    "
-                    >
-                    </animate>
-                </path>
-            </svg>
+            <Image
+                className={styles.wave__desktop}
+                alt="Wave"
+                src={desktop}
+                // layout="fill"
+                // objectFit="cover"
+                // quality={40}
+                // priority
+            />
         </>
 
     )
